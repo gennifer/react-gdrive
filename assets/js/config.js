70fc9fb67e908fc7016dc12a755a44e0f00a7e88
gennifer <gennifer.gates@flashtalking.com>
2018-01-20 22:15:49 -0600

    Initial Commit

---
+++ assets/js/config.js
@@ -0,0 +1,15 @@
+configReactDriveCms = {
+    author: 'gennifer gates',
+    /**
+     * Add the Id for the Google Sheets Dashboard file
+     */
+    dashboardId: '1rQeTY6zRK8oW3hThhjPLzMmAuPOLEPCILfh6ctwdEzo',
+    /**
+     * Add the Url Id for the Google Web App Script to forward email from the Contact Form
+     */
+    sendMailUrlId: 'https://script.google.com/macros/s/AKfycbzN70clYpX1QZro_Y2855D8ux2_6c8VaZHMCVpdpssOsZimioM/exec',
+    /**
+     * Add your Disqus shortname
+     */
+    shortname: 'gennifer'
+};
